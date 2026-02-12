01ed3dc6e1340f8c39ece2842ac9de02bf34dd90
Prateek Bhatt <bhattprateek999@gmail.com>
2026-02-12 13:07:26 +0000

    Add persistent storage for database

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./data.db"
+DATABASE_URL = "sqlite:///app/data.db"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
